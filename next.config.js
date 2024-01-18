9aa75fbbb1b2ff5c0fa38be83e56d6f839e9c05e
Akshay Sharma <86650974+tcs-asharma@users.noreply.github.com>
2024-01-18 23:51:08 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assestPrefix: 'https://nextjs-boilerplate-three-alpha-57.vercel.app/'
+  assetPrefix: 'https://nextjs-boilerplate-three-alpha-57.vercel.app/'
 }
 
 module.exports = nextConfig
