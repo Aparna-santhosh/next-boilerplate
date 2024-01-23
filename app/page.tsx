6bc4c8840ab36d1cc333426aa2ec9f54b749f362
Aparna-santhosh <85827448+Aparna-santhosh@users.noreply.github.com>
2024-01-23 20:27:16 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <h1>Header from Next JS app!</h1>
+      <h1>Header from Next JS app</h1>
     </main>
   )
 }
